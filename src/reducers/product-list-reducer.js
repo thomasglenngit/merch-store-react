4af3c0b9695aca01c36072305352e9f8ebe04875
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-14 15:36:33 -0600

    add passing test for productListReducer.
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -1,3 +1,16 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, description, price, id } = action;
+  switch (action.type) {
+  case 'ADD_TICKET':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        description: description,
+        price: price,
+        id: id
+      }
+    });
+    default:
+      return state;
+  }
 };
