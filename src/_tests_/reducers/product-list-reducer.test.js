1c60ffeac4c4ffd4b137658a3334cd8f1c2478e3
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-14 15:18:18 -0600

    add failed object test.
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- src/_tests_/reducers/product-list-reducer.test.js
+++ src/_tests_/reducers/product-list-reducer.test.js
@@ -1,6 +1,6 @@
 import productListReducer from '../../reducers/product-list-reducer';
 
-describe('ticketListReducer', () => {
+describe('productListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(productListReducer({}, { type: null })).toEqual({});
   });
