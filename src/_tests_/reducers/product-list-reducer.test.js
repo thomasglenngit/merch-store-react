7bae776edf5dd66384a25fa7ad1dc5769ab15039
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-14 15:31:44 -0600

    add failing test for productListReducer.
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- src/_tests_/reducers/product-list-reducer.test.js
+++ src/_tests_/reducers/product-list-reducer.test.js
@@ -1,7 +1,37 @@
 import productListReducer from '../../reducers/product-list-reducer';
 
 describe('productListReducer', () => {
+  
+  let action;
+  const productData = {
+    name: 'Air Ryan',
+    description: 'Shoes',
+    price: '$50.00',
+    id: 1
+  };
+  
+  
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(productListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new product data to masterList', () => {
+    const { name, description, price, id } = productData;
+    action = {
+      type: 'ADD_TICKET',
+      name: name,
+      description: description,
+      price: price,
+      id: id
+    };
+
+    expect(productListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        description: description,
+        price: price,
+        id: id
+      }
+    });
   });
 });
